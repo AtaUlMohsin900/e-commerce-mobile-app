6dc719a24b9a8393bc6c1a6bc810b357f1535d4a
AtaUlMohsin900 <ataulmohsin900@gmail.com>
2025-12-25 20:12:28 +0500

    update

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -3,9 +3,7 @@
   dotenv.config()
 
   export const ENV ={
-    NODE_ENV:process.env.NODE_ENV,
-    PORT_ENV:process.env.PORT,
-    DB_URL:process.env.DB_URL
- 
-
-  }
+    NODE_ENV: process.env.NODE_ENV,
+    PORT: process.env.PORT,
+    DB_URL: process.env.DB_URL,
+ }
