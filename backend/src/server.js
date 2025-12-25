927f9d300a74a4d55de00580a2be05fe325dc1e7
ataulmohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2025-12-25 14:20:23 +0000

    add routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,8 +7,15 @@ app.get('/api/health', (req,res)=>{
   res.status(200).json({message:"Success"})
 
 });
+
+
+// make app rady for deployment
 if(ENV.NODE_ENV === 'production'){
   app.use(express.static(path.join(__dirname, '../admin/dist')))
+
+  app.get('/{*any}',(req, res)=>{
+    res.sendFile(path.join(__dirname, '../admin', 'dist', 'index.html'));
+  })
+
 }
 app.listen(ENV.PORT  , ()=>console.log('Server is up and running'));
-// make app for deployment
